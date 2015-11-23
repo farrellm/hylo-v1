d78c9ab2cefd30798dc439b596386ca26e9e3170
Matthew Farrell <farrellm@alum.mit.edu>
2015-11-22 22:51:08 -0500

    fix basic type inference

--- src/hylo/core.clj
+++ src/hylo/core.clj
@@ -22,111 +22,123 @@
                                   :return :a
                                   :arguments [:a]}
 
-                                 ;; #'if
-                                 ;; {:class :fn
-                                 ;;  :constraints {}
-                                 ;;  :return :a
-                                 ;;  :arguments [Boolean :a :a]}
+                                 'if
+                                 {:class :fn
+                                  :constraints {}
+                                  :return :a
+                                  :arguments [Boolean :a :a]}}})
 
-                                 ;; #'id2
-                                 ;; (let [a (gensym)
-                                 ;;       b (gensym)]
-                                 ;;   {:class :fn
-                                 ;;    :constraints {}
-                                 ;;    :return a
-                                 ;;    :arguments [a b]})
+(def type-keywords
+  (->> (int \a) (iterate inc) (map (comp keyword str char))))
 
-                                 ;; #'id3
-                                 ;; (let [a (gensym)]
-                                 ;;   {:class :fn
-                                 ;;    :constraints {}
-                                 ;;    :return a
-                                 ;;    :arguments [a a]})
-                                 }})
+(defn primitive? [expr]
+  (or (instance? Boolean expr)
+      (number? expr)
+      (string? expr)
+      (keyword? expr)))
 
-(defn context-contains? [context key]
+(defn context-contains? [context k]
   (cond
-    (contains? (:assumptions context) key)
+    (contains? (:assumptions context) k)
     true
 
     (:parent context)
-    (context-contains? (:parent context) key)
+    (context-contains? (:parent context) k)
 
     :else
     false))
 
-(defn context-get [context key]
+(defn context-get [context k]
   (cond
-    (contains? (:assumptions context) key)
-    ((:assumptions context) key)
+    (contains? (:assumptions context) k)
+    (let [value ((:assumptions context) k)]
+      (or (context-get context value) value))
 
     (:parent context)
-    (context-contains? (:parent context) key)
+    (context-get (:parent context) k)
 
     :else
     nil))
 
-(defn context-set [context key type]
+(defn context-set [context k t]
   (cond
-    (contains? (:assumptions context) key)
-    (let [cur (context-get context key)]
+    (contains? (:assumptions context) k)
+    (let [cur (context-get context k)]
       (cond
         (or (nil? cur) (symbol? cur))
-        (update-in context [:assumptions key] (constantly type))
+        (update-in context [:assumptions k] (constantly t))
 
-        (= type cur)
+        (= t cur)
         context
 
         :else
-        (throw (Exception. (str "Type mismatch: expected [" type "], found [" cur "]")))))
+        (throw (Exception. (str "Type mismatch: expected [" t "], found [" cur "]")))))
 
     (:parent context)
-    (assoc context :parent (context-set (:parent context) key type))
+    (update-in context [:parent] context-set k t)
 
     :else
-    (throw (Exception. (str "Missing key: " key)))))
+    (throw (Exception. (str "Missing key: " k)))))
 
-(declare unify)
+(defn context-specify [context k t]
+  (if-let [v (context-get context k)]
+    (if (= v t) context
+        (throw (Exception. (str "Type " k " is " v ", cannot be " t))))
+    (let [a (into {} (map (fn [[q v :as e]] (if (= v k) [q t] e))
+                          (:assumptions context)))]
+      (if (contains? (:assumptions context) k)
+        {:assumptions (assoc a k t)
+         :parent (:parent context)}
+        {:assumptions a
+         :parent (context-specify (:parent context) k t)}))))
 
-(defn unify-current [context a b]
-  "a is defined on this level, so be is either on this or a parent
-  level, so set a to equal b"
-  (let [cur-a (context-get context a)]
-    (cond
-      (nil? cur-a)
-      (context-set context a (or (context-get context b) b))
-
-      (symbol? cur-a)
-      (let [ctx-prime (unify cur-a b)]
-        (context-set ctx-prime a (or (context-get ctx-prime b) b)))
-
-      :else
-      (context-set context b cur-a))))
+(defn context-sort [context a b]
+  (cond
+    (contains? (:assumptions context) a) [a b]
+    (contains? (:assumptions context) b) [b a]
+    :else (context-sort (:parent context) a b)))
 
 (defn unify [context a b]
+  (prn [:unify a b])
   (cond
-    (and (class? a) (class? b))
-    (if (= a b) context
-        (throw (Exception. (str "Type mismatch: expected [" a "], found [" b "]"))))
+    (= a b)
+    context
 
-    (class? a) (context-set context b a)
-    (class? b) (context-set context a b)
+    (and (class? a) (symbol? b))
+    (if-let [b-val (context-get context b)]
+      (unify context a b-val)
+      (context-specify context b a))
 
-    (contains? (:assumptions context) a) (unify-current context a b)
-    (contains? (:assumptions context) b) (unify-current context b a)
+    (and (symbol? a) (class? b))
+    (unify context b a)
 
-    (:parent context) (unify (:parent context) a b)
+    (and (symbol? a) (symbol? b))
+    (let [a-val (context-get context a)
+          b-val (context-get context b)]
+      (prn [a-val b-val])
+      (prn context)
+      (cond
+        (and (nil? a-val) (nil? b-val))
+        (apply context-specify context (context-sort context a b))
 
-    :else (throw (Exception. (str "Could not find [" a "] or [" b "] to unify")))))
+        (and a-val (nil? b-val))
+        (context-specify context b a-val)
+
+        (and (nil? a-val) b-val)
+        (context-specify context a b-val)
+
+        (and a-val b-val)
+        (apply context-specify context (context-sort context a-val b-val))))
+
+    :else
+    (throw (Exception. (str "Could not unify [" a "] with [" b "]")))))
 
 (declare type-of-form)
 (declare type-of-apply)
 
 (defn type-of [context expr]
-  (cond (or (instance? Boolean expr)
-            (number? expr)
-            (string? expr)
-            (keyword? expr))
+  (prn :type-of expr)
+  (cond (primitive? expr)
         {:type (class expr)
          :context context}
 
@@ -137,67 +149,70 @@
         {:type (context-get context expr)
          :context context}
 
-        (and (symbol? expr) (context-contains? context (resolve expr)))
-        {:type (context-get context (resolve expr))
+        (and (symbol? expr)
+             (context-contains? context (or (resolve expr) expr)))
+        {:type (context-get context (or (resolve expr) expr))
          :context context}
 
         :else
         (throw (Exception. (str "Unknown type: [" expr "]")))))
 
-(defn type-of-form [context f args]
+(defn type-of-form [parent-context f args]
   (cond
     (= f 'fn)
-    :yay
+    (let [[ps expr] args
+          mapping (zipmap ps (repeatedly gensym))
+          ctx-prime {:parent parent-context
+                     :assumptions (merge mapping
+                                         (zipmap (vals mapping) (repeat nil)))}
+          {:keys [type context]} (type-of ctx-prime expr)
+          free (remove (partial context-get context) (vals mapping))
+          free-mapping (zipmap free type-keywords)
+
+          calc-type #(if (class? %) %
+                         (or (context-get context %)
+                             (free-mapping %)
+                             %))]
+      {:type {:class :fn
+              :constraints {}
+              :return (calc-type type)
+              :arguments (map (comp calc-type (partial context-get context)) ps)}
+       :context (:parent context)}
+      )
 
     :else
-    (type-of-apply context f args)))
+    (type-of-apply parent-context f args)))
 
 (defn type-of-apply [parent-context f args]
   (let [f-type (:type (type-of parent-context f))
         free-types (->> (:arguments f-type) (filter keyword?) (into #{}))
-        mapping (zipmap free-types (repeatedly gensym))
+        mapping (zipmap free-types
+                        (map #(gensym (str f "_"(name %) "_")) free-types))
 
-        context {:parent parent-context
-                 :assumptions (zipmap (vals mapping)
-                                      (repeat nil))}
+        ctx {:parent parent-context
+             :assumptions (zipmap (vals mapping)
+                                  (repeat nil))}
 
         param-types (map #(or (mapping %) %) (:arguments f-type))
-        arg-types (map (comp :type (partial type-of context)) args)
+
+        {:keys [arg-types ctx-prime]}
+        (reduce (fn [{:keys [arg-types ctx-prime]} arg]
+                  (let [{:keys [type context]} (type-of ctx-prime arg)]
+                    {:arg-types (conj arg-types type)
+                     :ctx-prime context}))
+                {:ctx-prime ctx
+                 :arg-types []}
+                args)
 
         _ (if-not (and (map? f-type) (= (:class f-type) :fn))
             (throw (Exception. (str "Cannot apply '" f "' of type [" f-type "]"))))
 
         ctx-prime (reduce
                    (fn [c [a v]] (unify c a v))
-                   context
+                   ctx-prime
                    (map vector param-types arg-types))]
 
     {:type (if (keyword? (:return f-type))
              ((:assumptions ctx-prime) (mapping (:return f-type)))
              (:return f-type))
      :context (:parent ctx-prime)}))
-
-
-(type-of root-context '(fn [x] x))
-
-;; (:type (type-of root-context '(if true :a :b)))
-
-(:type (type-of root-context '(id 3.14)))
-(:type (type-of root-context '(id 8)))
-;; (type-of root-context '(id2 :a 8))
-;; (type-of root-context '(id3 :a :b))
-;; (type-of root-context '(id3 :a 8))
-
-(:type (type-of root-context '(sqrt 3.14)))
-;; (type-of root-context '(sqrt 3))
-
-;; (type-of root-context '(8 3.14))
-
-(:type (type-of root-context '3.14))
-(:type (type-of root-context 'sqrt))
-
-(:type (type-of root-context 'id))
-;; (type-of root-context 'id2)
-;; (type-of root-context 'id3)
-
-(:type (hylo (sqrt 3.14)))
